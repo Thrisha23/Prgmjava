4bbff828a6b47598a784aff71d087094fccd0462
Thrisha23 <147418119+Thrisha23@users.noreply.github.com>
2023-10-15 13:12:19 +0530

    Update VarArgs.java

--- VarArgs.java
+++ VarArgs.java
@@ -16,7 +16,7 @@ public class VarArgs {
 public static void main(String[]args)
 {
    int a= minvalue(4,45,34,2,7,8,967,4,5,67,457,3,1,56,7856,45,6);
-   System.out.println("The smallest value of q var:"+a);
+   System.out.println("The smallest value of a var:"+a);
    int y=minvalue(-2,-78,56,1,-889,0,567,-456567,79,11);
     System.out.print("The smallest value of y:"+y);
 }
