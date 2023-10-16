391e3c56003eabcd755fe6a63c57b6e42ca39909
Thrisha23 <thrishakomu@gmail.com>
2023-10-16 20:43:44 +0530

    Update

--- Overloadvargs.java
+++ Overloadvargs.java
@@ -4,7 +4,7 @@ public class Overloadvargs {
     public static void main(String[]args)
     {
         demo(2,4,3,4,3,55);
-        demo("Laptop Watch");
+        demo("Laptop Watch","neeyu","nanum","sen dhae","selum","neramae");
     }
     static void demo(int...v)
     {
