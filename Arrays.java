750450e017d9276911772935da7116f42977dbc6
Thrisha23 <thrishakomu@gmail.com>
2023-10-21 01:09:52 +0530

    Create array

---
+++ Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays {
+    public static void main(String[]args)
+    {
+       int arr[]={34,5,5,87,98,0,1}; 
+        for (int i = 0; i < arr.length; i++) {
+           System.out.println(arr[i]); 
+        }
+
+
+    }
+
+    public static char[] toString(String[] str) {
+        return null;
+    }
+}
